c560fc592c457f23d2123969471518f32468a9c5
Sowndarya <sowndarya1908@gmail.com>
2025-12-27 18:12:07 +0530

    contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 
+import { Link } from "react-router-dom";
 
 import { useNavigate } from "react-router-dom";
 
@@ -1132,9 +1133,9 @@ html, body{
         <div className="home-faq-cta">
           <h3>Still have questions?</h3>
           <p>Talk directly with our experts and get a custom growth plan.</p>
-          <button onClick={() => window.location.href="/contact"}>
+         <Link to="/contact"><button>
             BOOK FREE CONSULTATION →
-          </button>
+          </button></Link> 
         </div>
       </div>
     </section>
